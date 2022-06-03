78b43b2499770b71181db317332bd48a70bcff37
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-03 18:58:56 +0200

    routes and main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainHomeComponent } from './views/main-home/main-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'main-home', pathMatch: 'full'},
+  {path: 'main-home', component: MainHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavbarComponent } from './core/layout/main-navbar/main-navbar.component';
+import { MainHomeComponent } from './views/main-home/main-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent,
+    MainHomeComponent
   ],
   imports: [
     BrowserModule,
